dbaaf9bcc330e6b3485a9691ba000a4e4ddaf1ba
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-03-21 12:47:22 +0100

    Bootstrap for a real quick start
    For users that just want to try OpenRISC this is a real quickstart. It
    downloads all prebuilt binary tools.

---
+++ bootstrap-quick-start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+make toolchain-baremetal
+make openocd-download
+make fusesoc-download
+make or1ksim-download
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++"
+echo "++ Quick Start Bootstrap completed"
+echo "++ Run the following command and you are ready"
+echo "++     source environment.sh"
