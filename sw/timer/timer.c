a8c72f94bd5957b41c42c3791e88e32980a2f9a7
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-03-21 14:00:08 +0100

    SW: Handle gpio only on the board

--- sw/timer/timer.c
+++ sw/timer/timer.c
@@ -3,24 +3,40 @@
 
 #include <stdio.h>
 
+#ifdef DE0_NANO
 char* gpio_base = (char*) 0x91000000;
 
 unsigned char dat = 0x00;
 
-extern void *_or1k_impure_ptr;
-
-int main(void)
-{
-
+static void gpio_init() {
   /* Set the GPIO to all out */
   char dir = 0xff;
   *(gpio_base+1) = dir;
   /* Initialise with the first data value */
   *(gpio_base+0) = dat;
+}
 
+static void gpio_increment() {
+  *(gpio_base+0) = (++dat)%256;
+}
+#else
+static void gpio_init() {
+
+}
+
+static void gpio_increment() {
+
+}
+#endif
+
+
+int main(void)
+{
   uint32_t ticks = 0;
   uint32_t timerstate;
   or1k_timer_init(100);
+
+  gpio_init();
 
   or1k_timer_enable();
   while (1) {
@@ -32,7 +48,7 @@ int main(void)
     if (ticks == 100) {
       printf("A second elapsed\n");
       // Increment the GPIO counter
-      *(gpio_base+0) = (++dat)%256;
+      gpio_increment();
       or1k_timer_reset_ticks();
       ticks = 0;
     }
